97ea2d6ae54c029e7c835292baddd41287297dfe
RajatG17 <68469636+RajatG17@users.noreply.github.com>
2026-04-21 21:57:50 -0400

    chore: remove debug print statement and update backend dependencies

--- backend/db/database.py
+++ backend/db/database.py
@@ -4,8 +4,6 @@ from sqlalchemy.pool import NullPool
 from core.config import settings
 from typing import AsyncGenerator
 
-
-print(settings.DATABASE_URL)
 engine = create_async_engine(settings.DATABASE_URL, echo=False, poolclass=NullPool)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
